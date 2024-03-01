54d09b2a587d0cad895b20c1b576fcfbdf874840
cyberdevilz90 <ahmadmahmudi2000@gmail.com>
2024-03-01 08:38:05 +0700

    penambahan background dan pembuatan inti konten

---
+++ src/components/ProductDisplay.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="bg-container">
+      <img class="bg-pattern" src="../assets/bg-pattern.png" alt="bg-pattern" />
+      <div class="content">
+        <div class="image-product-container">
+          <img
+            class="image-product"
+            src="../assets//img-product.png"
+            alt="product"
+          />
+        </div>
+        <div>
+          <p>TITLE PRODUCT</p>
+          <div>
+            <p></p>
+            <p></p>
+            <img src="" alt="" />
+          </div>
+          <div></div>
+          <p></p>
+          <div></div>
+          <p></p>
+          <div>
+            <div>
+              <p></p>
+            </div>
+            <div>
+              <p></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
